106531252775e4a6dbc2a06d217a0f23cd4db809
CristiManolescu <m_cristian_s96@yahoo.com>
2024-02-04 14:25:36 +0200

    Feat: Broadcast message

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
-// 12:54
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+//23:24
+const socket = io.connect("http://localhost:3001");
+
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
   return (
     <div className="App">
-      <input placeholder="Message..." />
-      <button>Send Message</button>
+      <input
+        placeholder="Message..."
+        onChange={(event) => setMessage(event.target.value)}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      {messageReceived && <p>{`Mesajul primit este: ${messageReceived}`}</p>}
     </div>
   );
 }
