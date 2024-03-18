8b5431c811a99c72e122d08c90a1a04f477a23ed
CristiManolescu <m_cristian_s96@yahoo.com>
2024-03-18 12:41:38 +0200

    Feat: Add theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,48 @@
-import "./App.css";
+import { Container, Typography } from "@mui/material";
 import io from "socket.io-client";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 //27:25
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  const [room, setRoom] = useState("");
+  // const [room, setRoom] = useState("");
 
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
+  // const [message, setMessage] = useState("");
+  // const [messageReceived, setMessageReceived] = useState("");
 
-  const joinRoom = () => {
-    if (room !== "") {
-      socket.emit("join_room", room);
-    }
-  };
+  // const joinRoom = () => {
+  //   if (room !== "") {
+  //     socket.emit("join_room", room);
+  //   }
+  // };
 
-  const sendMessage = () => {
-    socket.emit("send_message", { message, room });
-  };
+  // const sendMessage = () => {
+  //   socket.emit("send_message", { message, room });
+  // };
 
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-    });
-  }, [socket]);
+  // useEffect(() => {
+  //   socket.on("receive_message", (data) => {
+  //     setMessageReceived(data.message);
+  //   });
+  // }, [socket]);
 
   return (
-    <div className="App">
-      <input
-        placeholder="Room Number"
-        onChange={(event) => setRoom(event.target.value)}
-      />
-      <button onClick={joinRoom}>Join Room</button>
-      <input
-        placeholder="Message..."
-        onChange={(event) => setMessage(event.target.value)}
-      />
-      <button onClick={sendMessage}>Send Message</button>
-      {messageReceived && <p>{`Mesajul primit este: ${messageReceived}`}</p>}
-    </div>
+    <Container sx={{ bgcolor: "tomato", height: "100vh" }}>
+      <Typography sx={{ p: 1 }}>Test</Typography>
+    </Container>
+    // <div className="App">
+    //   <input
+    //     placeholder="Room Number"
+    //     onChange={(event) => setRoom(event.target.value)}
+    //   />
+    //   <button onClick={joinRoom}>Join Room</button>
+    //   <input
+    //     placeholder="Message..."
+    //     onChange={(event) => setMessage(event.target.value)}
+    //   />
+    //   <button onClick={sendMessage}>Send Message</button>
+    //   {messageReceived && <p>{`Mesajul primit este: ${messageReceived}`}</p>}
+    // </div>
   );
 }
 
