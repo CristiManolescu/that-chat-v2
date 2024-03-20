bfeb4c15a52f9da96aa0a789601de65508eb67e7
CristiManolescu <m_cristian_s96@yahoo.com>
2024-03-20 15:25:37 +0200

    Design fix

--- client/src/components/MessageBox.jsx
+++ client/src/components/MessageBox.jsx
@@ -1,43 +1,46 @@
-import { Box, TextField, FormControl, Stack } from "@mui/material";
+import { Box, TextField, Stack } from "@mui/material";
 import React, { useState } from "react";
 
 const MessageBox = () => {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
 
-  const handleAction = (e) => {
-    e.preventDefault();
+  const handleAction = () => {
     setMessages([message, ...messages]);
     setMessage("");
   };
 
   return (
-    <form onSubmit={handleAction}>
-      <Stack
+    <Stack
+      sx={{
+        border: "1px solid",
+        width: 1,
+        p: 1,
+        m: 1,
+      }}
+    >
+      <Box
         sx={{
           border: "1px solid",
+          borderRadius: "5px",
           height: 1,
+          p: 1,
+          m: 1,
         }}
       >
-        <Box
-          sx={{
-            border: "1px solid",
-            borderRadius: "5px",
-            p: 1,
-            m: 1,
-          }}
-        >
-          {messages}
-        </Box>
-        <TextField
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-          variant="outlined"
-          placeholder="Enter your message here"
-          sx={{ m: 1 }}
-        />
-      </Stack>
-    </form>
+        {messages}
+      </Box>
+      <TextField
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}
+        variant="outlined"
+        placeholder="Enter your message here"
+        sx={{ m: 1 }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleAction();
+        }}
+      />
+    </Stack>
   );
 };
 
