ec1eaf544c5133c04ef541e89401bc5e130faf27
cristimanolescu <m_cristian_s96@yahoo.com>
2024-03-24 13:18:04 +0200

    To be done

--- client/src/components/ChatRooms.jsx
+++ client/src/components/ChatRooms.jsx
@@ -15,10 +15,19 @@ const ChatRooms = () => {
   const [joinUsers, setJoinUsers] = useState([]);
 
   const joinRoom = (chatRoom) => {
+// TODO: UI intrat pe chat + conectare la socket
+
     //socket.emit("join_room", chatRoom); // change the Chat Room
     //console.log(chatRoom);
-    setJoinUsers([{[chatRoom]: userName}, ...joinUsers]);
-    console.log(joinUsers);
+    
+    console.log(joinUsers)
+
+/*  UI => Daca e acelasi room iar userul e diferit => POATE intra pe room
+    userName => e un Array de useri conectati
+    joinUsers => un array de obiecte => Fiecare obiect are un nume de camera + un array de useri conectati
+*/
+    if(joinUsers.findIndex(x => x.roomName === chatRoom && x.userName === userName) === -1)
+      setJoinUsers([{roomName: chatRoom, userName: userName}, ...joinUsers]);
   };
 
   return (
