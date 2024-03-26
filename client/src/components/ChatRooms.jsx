691cf0b5df84f5128fabc7709fa97764c1d93056
cristimanolescu <m_cristian_s96@yahoo.com>
2024-03-26 14:42:35 +0200

    Removed initial functionality

--- client/src/components/ChatRooms.jsx
+++ client/src/components/ChatRooms.jsx
@@ -11,7 +11,7 @@ import { rooms } from "../utils/data";
 const socket = io.connect("http://localhost:3001");
 
 const ChatRooms = () => {
-  const userName = "testUser";
+  const userName = "altu";
   
   const [users, setUsers] = useState([]);
   const [room, setRoom] = useState();
@@ -22,35 +22,6 @@ const ChatRooms = () => {
 
     //socket.emit("join_room", chatRoom); // change the Chat Room
     //console.log(chatRoom);
-    
-
-    if(joinUsers.findIndex(x => x.roomName === chatRoom) === -1){ // DACA ROOM NU EXISTA
-      setRoom(chatRoom); // se adauga room
-      setUsers([userName, ...users]); // se adauga user
-      setJoinUsers([{roomName: room, onlineUsers: users}]);
-    } else { // DACA ROOM EXISTA
-      console.log(joinUsers[0].onlineUsers);
-      // if(joinUsers.findIndex(x => x.usersName === userName) === -1){ // DACA USERUL NU ESTE PE ROOM
-      //   setUsers([userName, ...users]); // se adauga user
-      //   setJoinUsers([{roomName: room, onlineUsers: users}]);
-      // }
-    }
-    console.log(joinUsers);
-
-
-    // if(joinUsers.findIndex(x => x.roomName === chatRoom) !== -1){ // DACA CAMERA EXISTA
-    //   if(joinUsers.findIndex(x => x.usersName === userName) === -1){ // DACA USERUL NU ESTE PE ROOM
-    //     setUsers([userName ,...users]);
-    //     setJoinUsers([{roomName: chatRoom, usersName: users}]);
-    //   }
-    // }
-
-/*  UI => Daca e acelasi room iar userul e diferit => POATE intra pe room
-    userName => e un Array de useri conectati
-    joinUsers => un array de obiecte => Fiecare obiect are un nume de camera + un array de useri conectati
-*/
-    // if(joinUsers.findIndex(x => x.roomName === chatRoom && x.userName === userName) === -1)
-    //   setJoinUsers([{roomName: chatRoom, userName: userName}, ...joinUsers]);
 
   };
 
